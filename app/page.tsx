01207a03c7b7dee0fa36dc7ad8dfdf3ac76beb1c
장은아 <aa@Eunas-MacBookPro.local>
2025-10-31 04:09:24 +0900

    Fix: mobile padding

--- app/page.tsx
+++ app/page.tsx
@@ -11,21 +11,22 @@ export default function Home() {
           width={100}
           height={20}
           priority
+          className="pb-8 sm:pb-0"
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-s text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
           PDF 창, 챗봇 창을 번갈아가며 공부하고 있나요?
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-          이제 수업자료를 보며 바로 AI에게 질문하세요.<br/>
-          페이지별 질문과 저장으로 공부 흐름이 끊기지 않습니다.<br/>
+          이제 수업자료를 보며 바로 AI에게 질문하세요. <br className="hidden sm:block"/>
+          페이지별 질문, 대화 저장으로 공부 흐름이 끊기지 않습니다.<br/>
           <br/>
           
-          2025년 12월<br/>Coming Soon... 
+          2025년 12월 <br className="hidden sm:block"/>Coming Soon... 
           </p>
         </div>
-          <div>
-            <button className="bg-black hover:bg-orange-700 transition-colors duration-300 text-white px-4 py-2 rounded-full">
+          <div className="pt-8 sm:pt-0">
+            <button className="bg-black hover:bg-orange-700 transition-colors duration-300 text-white px-4 py-2 rounded-full ">
               <a href="https://www.notion.so/kaist-students/A-Mate-AI-29b14badf5ac804f8481e7fbed15a740?source=copy_link" target="_blank" rel="noopener noreferrer">
               GDGoC Notion
               </a>
