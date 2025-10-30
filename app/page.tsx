324df4f378050bca7c238808a343a3376120a375
장은아 <aa@Eunas-MacBookPro.local>
2025-10-31 04:10:43 +0900

    Fix mobile padding

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,12 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert"
+          className="dark:invert pb-8 sm:pb-0"
           src="/aplusmatelogo.svg"
           alt="logo"
           width={100}
           height={20}
           priority
-          className="pb-8 sm:pb-0"
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-s text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
