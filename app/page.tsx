21d2e4a56c1f7b144eb06faa3db164bdc872aa00
장은아 <aa@Eunas-MacBookPro.local>
2025-11-05 23:42:38 +0900

    Feat: next auth google login

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 'use client';
 import Image from 'next/image';
 import { useState, useRef } from 'react';
+import { signIn } from 'next-auth/react';
 
 export default function Home() {
   const [isPopupOpen, setIsPopupOpen] = useState(true);
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isDragging, setIsDragging] = useState(false);
 
-  const handleFileUpload = async (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const file = event.target.files?.[0];
-    if (!file) return;
-
+  // 파일 처리 로직을 공통 함수로 분리
+  const processFile = async (file: File) => {
     console.log('선택된 파일:', file.name, file.type, file.size);
 
     if (file.type !== 'application/pdf') {
@@ -44,57 +42,153 @@ export default function Home() {
     }
   };
 
+  const handleFileUpload = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+    await processFile(file);
+  };
+
   const handleDivClick = () => {
     fileInputRef.current?.click();
+  };
+
+  // drag drop 핸들러
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(false);
+  };
+
+  const handleDrop = async (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(false);
+
+    const file = e.dataTransfer.files?.[0];
+    if (!file) return;
+
+    await processFile(file);
   };
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#F2F2F2] font-sans dark:bg-black">
       <Image
         className="fixed top-8 left-8 z-10"
-        src="/aplusmatelogo.svg"
+        src="/logo.svg"
         alt="logo"
-        width={100}
-        height={20}
+        width={130}
+        height={30}
         priority
       />
-      <nav className="fixed top-36 left-8 w-1/6 font-ibm-plex-mono font-medium">
-        <ul className="flex flex-col gap-2">
-          <li className="bg-[#D9D9D9] px-4 py-2 rounded-lg w-full">
-            Documents
-          </li>
-          <li className=" px-4 py-2 rounded-lg w-full cursor-not-allowed">
-            Bookmarks
-          </li>
-        </ul>
+
+      <nav
+        className={`fixed h-full flex flex-col ${
+          isPopupOpen
+            ? 'w-1/3  top-0 left-0 justify-center '
+            : 'w-1/6 top-36 left-8'
+        } `}
+      >
+        {isPopupOpen && (
+          <div className="flex flex-col gap-8 p-8">
+            <h1 className="max-w-s text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+              PDF 창, 챗봇 창을 <br />
+              번갈아가며 공부하고 있나요?
+            </h1>
+            <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+              이제 수업자료에서 바로 AI에게 질문하세요.{' '}
+              <br className="hidden sm:block" />
+              페이지별 질문, 대화 저장으로 공부 흐름이 끊기지 않습니다.
+            </p>
+          </div>
+        )}
+
+        {!isPopupOpen && (
+          <ul className="flex flex-col gap-2 font-ibm-plex-mono font-medium">
+            <li className="bg-[#D9D9D9] px-4 py-2 rounded-lg w-full">
+              Documents
+            </li>
+            <li className="text-zinc-300 px-4 py-2 rounded-lg w-full cursor-not-allowed">
+              Bookmarks
+            </li>
+            <li className="text-zinc-300 px-4 py-2 rounded-lg w-full cursor-not-allowed">
+              Shop
+            </li>
+          </ul>
+        )}
       </nav>
 
       <section
         className={`fixed top-20 ${
-          isPopupOpen ? 'left-0 w-1/2' : 'left-1/5 w-4/5'
+          isPopupOpen ? 'left-1/3 w-2/3' : 'left-1/5 w-4/5'
         } rounded-48  h-full flex flex-col items-center justify-center gap-12 p-8 sm:items-start transition-all duration-600`}
       >
-        <div className="rounded-[48px] bg-white dark:bg-black w-full h-full flex flex-col justify-center p-8 gap-6 text-center sm:items-start sm:text-left">
+        <div className="rounded-[48px] bg-white dark:bg-black w-full h-full p-8 gap-6 text-center items-start text-left flex flex-col sm:justify-center sm:items-center">
           {isPopupOpen && (
-            <>
-              <h1 className="max-w-s text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-                PDF 창, 챗봇 창을 <br />
-                번갈아가며 공부하고 있나요?
+            <div className="flex flex-col items-center justify-center w-1/2 pb-16">
+              <h1 className="max-w-s text-2xl font-ibm-plex-mono font-medium leading-10 tracking-tight text-black dark:text-zinc-50 mb-16">
+                Create Your Space
               </h1>
-              <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-                이제 수업자료에서 바로 AI에게 질문하세요.{' '}
-                <br className="hidden sm:block" />
-                페이지별 질문, 대화 저장으로 공부 흐름이 끊기지 않습니다.
-              </p>
-              <div className="mt-12 sm:pt-0">
+              <div className="flex flex-col gap-12 w-full">
+                <div className="flex flex-col gap-4">
+                  <label className="text-sm font-ibm-plex-mono text-black dark:text-zinc-50">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    // value={name}
+                    // onChange={(e) => setName(e.target.value)}
+                    className="font-ibm-plex-mono text-sm  pb-2 border-b-2 border-black focus:outline-none bg-white dark:bg-black text-black dark:text-zinc-50"
+                    placeholder="Enter your name"
+                  />
+                </div>
+                <div className="flex flex-col gap-4">
+                  <label className="text-sm font-ibm-plex-mono text-black dark:text-zinc-50">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    // value={password}
+                    // onChange={(e) => setPassword(e.target.value)}
+                    className="font-ibm-plex-mono text-sm  pb-2 border-b-2 border-black focus:outline-none bg-white dark:bg-black text-black dark:text-zinc-50"
+                    placeholder="Enter your password"
+                  />
+                </div>
+              </div>
+              <div className="mt-12 sm:pt-0 w-full">
                 <button
                   onClick={() => setIsPopupOpen(false)}
-                  className="font-ibm-plex-mono bg-black hover:bg-[#1F4E8B] transition-colors duration-300 text-white px-4 py-2 rounded-full font-semibold"
+                  className="w-full font-ibm-plex-mono bg-gray-600 hover:bg-gray-300 transition-colors duration-300 text-white px-4 py-2 rounded-full font-medium"
                 >
                   Get Started
                 </button>
               </div>
-            </>
+              <p className="text-sm mt-2 font-ibm-plex-mono font-medium text-zinc-500">
+                Already have an account? <a href="/login">Login</a>
+              </p>
+              <div className="mt-12 sm:pt-0 w-full hover:opacity-20 transition-opacity duration-300">
+                <button
+                  onClick={() =>
+                    signIn('google', { callbackUrl: '/dashboard' })
+                  }
+                  className="w-full font-ibm-plex-mono border-black border-1 transition-colors duration-300 text-black px-4 py-2 rounded-full font-medium"
+                >
+                  Continue with Google
+                </button>
+              </div>
+              <div className="mt-2 sm:pt-0 w-full hover:opacity-20 transition-opacity duration-300">
+                <button className="w-full font-ibm-plex-mono border-black border-1 transition-colors duration-300 text-black px-4 py-2 rounded-full font-medium">
+                  Continue with Apple
+                </button>
+              </div>
+            </div>
           )}
           {!isPopupOpen && (
             <>
@@ -108,9 +202,17 @@ export default function Home() {
               <div className="grid grid-cols-3 gap-4 w-full h-full">
                 <div
                   onClick={handleDivClick}
-                  className="hover:opacity-20 transition-opacity duration-300 w-full h-1/3 flex flex-col items-center justify-center gap-2 cursor-pointer"
+                  onDragOver={handleDragOver}
+                  onDragLeave={handleDragLeave}
+                  onDrop={handleDrop}
+                  className={`hover:opacity-20 transition-opacity duration-300 w-full h-1/3 flex flex-col items-center justify-center gap-2 cursor-pointer ${
+                    isDragging ? 'opacity-10 border-blue-800' : ''
+                  }`}
                 >
-                  <div className="w-full h-full border-2 border-black border-dotted rounded-[24px] flex items-center justify-center">
+                  <div
+                    className={`w-full h-full border-2 border-black border-dotted rounded-[24px] flex items-center justify-center 
+                     `}
+                  >
                     <svg
                       width="59"
                       height="59"
